79f5db9daadaf2084b6dd2b2a947398a5bc0a3a8
Hominhduc10899 <s3694653@rmit.edu.vn>
2021-01-03 17:27:54 +0700

    commit

---
+++ src/Screens/SigninScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SigninScreen(props) {
+    return <div className="signin">
+        <h1>Sign in</h1>
+    </div>
+}
+
+export default SigninScreen;
---
+++ src/data.js
@@ -0,0 +1,70 @@
+export default {
+    products:[
+        {
+            _id:'1',
+            name: 'Iphone 12',
+            category: 'Product 1',
+            image: '/images/ip1.png',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Smart-phone'
+        },
+        {
+            _id:'2',
+            name: 'Iphone 12 Mini',
+            category: 'Product 1',
+            image: '/images/ip1.png',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Smart-phone'
+        },
+        {
+            _id:'3',
+            name: 'Iphone 12 Pro',
+            category: 'Product 2',
+            image: '/images/ip1.png',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Smart-phone'
+        },
+        {
+            _id:'4',
+            name: 'Iphone 12 Pro Max',
+            category: 'Product 2',
+            image: '/images/ip1.png',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Smart-phone'
+        },    
+        {
+            _id:'5',
+            name: 'Iphone 12 Pro Max',
+            category: 'Product 2',
+            image: '/images/ip1.png',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Smart-phone'
+        },    
+        {
+            _id:'6',
+            name: 'Iphone 12 Pro Max',
+            category: 'Product 2',
+            image: '/images/ip1.png',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Smart-phone'
+        },     
+    ]
+}
